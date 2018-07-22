620e63b600741090f1937126eafa32edd12ed653
Rob Holland <rob@clearbit.com>
2018-07-22 23:38:53 +0100

    RDS -> Elasticache.

--- lib/redis/elasticache/failover.rb
+++ lib/redis/elasticache/failover.rb
@@ -8,13 +8,14 @@ class Redis
       ELASTICACHE_READONLY_ERROR   = "READONLY You can't write against a read only slave."
       ELASTICACHE_READONLY_MESSAGE = 'A write operation was issued to an ElastiCache slave node.'
 
-      # Amazon RDS supports failover, but because it uses DNS magic to point to
-      # the master node, TCP connections are not disconnected and we can issue
-      # write operations to a node that is no longer the master. Under normal
-      # conditions this should be interpreted as a `CommandError`, but with RDS
-      # replication groups, we should consider this a `BaseConnectionError` so we
-      # terminate the connection, reconnect and retry the operation with the
-      # correct node as the master accepting writes.
+      # Amazon Elasticache supports failover, but because it uses DNS magic to
+      # point to the master node, TCP connections are not disconnected and we
+      # can issue write operations to a node that is no longer the master. Under
+      # normal conditions this should be interpreted as a `CommandError`, but
+      # with Elasticache replication groups, we should consider this a
+      # `BaseConnectionError` so we terminate the connection, reconnect and
+      # retry the operation with the correct node as the master accepting
+      # writes.
 
       def read
         super
